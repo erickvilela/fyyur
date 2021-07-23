7b459df61928e3f5349db37cbb87f553fc2b7b07
Erick Vilela <vilela.erick@gmail.com>
2021-07-22 19:54:10 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -33,56 +33,14 @@ import time
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+
 
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
+from models import db
 migrate = Migrate(app,db)
-
-class Venue(db.Model):
-    __tablename__ = 'venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(400))
-    seeking_talent = db.Column(db.Boolean)
-    genres_list = db.Column(db.Text)
-    seeking_description = db.Column(db.String(500))
-
-
-class Artist(db.Model):
-    __tablename__ = 'artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres_list = db.Column(db.Text)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(250))
-    looking_venue = db.Column(db.Boolean)
-    seeking_desc = db.Column(db.String(500))
-
-
-class Show(db.Model):
-  __tablename__= 'shows'
-
-  id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
-  parent = relationship(Venue, backref=backref("venue", cascade="all,delete"))
-  start_time = db.Column(db.DateTime)
 
 
 #----------------------------------------------------------------------------#
---
+++ models.py
@@ -0,0 +1,54 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import load_only
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects import postgresql
+from sqlalchemy import func
+from sqlalchemy.orm import backref
+
+db = SQLAlchemy(app)
+
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(400))
+    seeking_talent = db.Column(db.Boolean)
+    genres_list = db.Column(db.Text)
+    seeking_description = db.Column(db.String(500))
+
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres_list = db.Column(db.Text)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(250))
+    looking_venue = db.Column(db.Boolean)
+    seeking_desc = db.Column(db.String(500))
+
+
+class Show(db.Model):
+  __tablename__= 'shows'
+
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+  parent = relationship(Venue, backref=backref("venue", cascade="all,delete"))
+  start_time = db.Column(db.DateTime)
